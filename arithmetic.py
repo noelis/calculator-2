40f19f489a90391b0d376513827142bc0811ca8b
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 15:27:11 -0700

    successful REPL for add and subtract

---
+++ arithmetic.py
@@ -0,0 +1,50 @@
+def add(num1, num2):
+	"""
+	adds two numbers in prefix format
+	"""
+	return num1 + num2
+
+def subtract(num1, num2):
+	"""
+	Subtracts the second number from the first 
+	in prefix format
+	"""
+	return num1 - num2
+
+def multiply(num1, num2):
+    """
+    multiplies 2 numbers together
+    """
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """
+    divides 2 numbers together
+    """
+    return float(num1) / num2
+
+def square(num1):
+    """
+    returns the square of the number
+    """
+    return num1 * num1
+
+
+def cube(num1):
+    """
+    returns the cube of the number
+    """
+    return num1 ** 3
+
+def power(num1, num2):
+    """
+    raises the first number by the second number
+    """
+    return num1 ** num2
+
+def mod(num1, num2):
+    """
+    returns the remainder when first number is divided by the second number
+    """
+    return num1 % num2
---
+++ calculator.py
@@ -0,0 +1,18 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+while True:
+	user_request = raw_input(" >> ")
+	tokens = user_request.split(" ")
+	if tokens[0] == "+":
+		print add(int(tokens[1]), int(tokens[2]))
+	elif tokens[0] == "-":
+		print subtract(int(tokens[1]), int(tokens[2]))
