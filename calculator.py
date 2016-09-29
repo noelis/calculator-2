2daab1591188d59e65aef4b329f6711a2c9f5f7a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 12:18:24 -0700

    Changed exception error for quitting program and added a condition to check mispellings of commmands.

--- calculator.py
+++ calculator.py
@@ -17,13 +17,16 @@ while True:
 		elif len(user_request) <= 1 or user_request[0] == " ":
 			print "Nice try! Please enter a valid number of inputs."	
 		elif user_request[0:6] == "square" or user_request[0:4] == "cube":
-			tokens = user_request.split()
-			operator,num1 = tokens
-			num1 = int(num1)
-			if operator == "square":
-				print square(num1)	
-			elif operator == "cube":
-				print cube(num1)
+			if user_request[6] == " " or user_request[4] == " ":
+				tokens = user_request.split()
+				operator,num1 = tokens
+				num1 = int(num1)
+				if operator == "square":
+					print square(num1)	
+				elif operator == "cube":
+					print cube(num1)
+			else:
+				print "Invalid command! Make sure you check your spelling."
 		else:
 			tokens = user_request.split()
 			operator,num1, num2 = tokens
@@ -40,10 +43,13 @@ while True:
 			elif operator == "pow":
 				print power(num1, num2)	
 			elif operator == "mod":
-				print mod(num1, num2)	
+				print mod(num1, num2)
+			else:
+				print "Invalid command! Please try again!"		
 	except ValueError:
 		print "Invalid command! Try again!"
 	except:
-		print "Unexpected Error:", exc_info()[0]	
+		print "Goodbye ! Exiting the calculator !"
+		break	
 			
 		
