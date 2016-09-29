f70dcdb0cf8551754b3fa3f63c27ab184b7e504f
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:18:38 -0700

    Handled exceptions and errors, added logic for spaces and invalid characters

--- calculator.py
+++ calculator.py
@@ -4,29 +4,46 @@ calculator.py
 Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
-
+from sys import exc_info
 from arithmetic import *
 
 
 # Your code goes here
 while True:
-	user_request = raw_input(" >> ")
-	tokens = user_request.split(" ")
-	if tokens[0] == "+":
-		print add(int(tokens[1]), int(tokens[2]))
-	elif tokens[0] == "-":
-		print subtract(int(tokens[1]), int(tokens[2]))
-	elif tokens[0] == "*":
-		print multiply(int(tokens[1]), int(tokens[2]))
-	elif tokens[0] == "/":
-		print divide(int(tokens[1]), int(tokens[2]))
-	elif tokens[0] == "square":
-		print square(int(tokens[1]))	
-	elif tokens[0] == "cube":
-		print cube(int(tokens[1]))
-	elif tokens[0] == "pow":
-		print power(int(tokens[1]), int(tokens[2]))	
-	elif tokens[0] == "mod":
-		print mod(int(tokens[1]), int(tokens[2]))	
-	elif tokens[0] == "quit" or tokens[0] == "q":
-		break
+	try:
+		user_request = raw_input(" >> ")
+		if user_request == "quit" or user_request == "q":
+			break
+		elif len(user_request) <= 1 or user_request[0] == " ":
+			print "Nice try! Please enter a valid number of inputs."	
+		elif user_request[0:6] == "square" or user_request[0:4] == "cube":
+			tokens = user_request.split()
+			operator,num1 = tokens
+			num1 = int(num1)
+			if operator == "square":
+				print square(num1)	
+			elif operator == "cube":
+				print cube(num1)
+		else:
+			tokens = user_request.split()
+			operator,num1, num2 = tokens
+			num1 = int(num1)
+			num2 = int(num2)
+			if operator == "+":
+				print add(num1, num2)
+			elif operator == "-":
+				print subtract(num1, num2)
+			elif operator == "*":
+				print multiply(num1, num2)
+			elif operator == "/":
+				print divide(num1, num2)
+			elif operator == "pow":
+				print power(num1, num2)	
+			elif operator == "mod":
+				print mod(num1, num2)	
+	except ValueError:
+		print "Invalid command! Try again!"
+	except:
+		print "Unexpected Error:", exc_info()[0]	
+			
+		
