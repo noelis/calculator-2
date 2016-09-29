a01c16d80495103e00245b23c5f93b3163e4facf
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 13:14:32 -0700

    Add function which takes multiple arguments

---
+++ arithmetic_2.py
@@ -0,0 +1,57 @@
+
+def add(num):
+    """
+    adds two numbers in prefix format
+    """
+    total = 0
+    for thing in num:
+        total = total + thing
+    return total 
+
+def subtract(num1, num2):
+    """
+    Subtracts the second number from the first 
+    in prefix format
+    """
+    return num1 - num2
+
+def multiply(num1, num2):
+    """
+    multiplies 2 numbers together
+    """
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """
+    divides 2 numbers together
+    """
+    return float(num1) / num2
+
+def square(num1):
+    """
+    returns the square of the number
+    """
+    return num1 * num1
+
+
+def cube(num1):
+    """
+    returns the cube of the number
+    """
+    return num1 ** 3
+
+def power(num1, num2):
+    """
+    raises the first number by the second number
+    """
+    return num1 ** num2
+
+def mod(num1, num2):
+    """
+    returns the remainder when first number is divided by the second number
+    """
+    return num1 % num2
+
+a = [1,2,3,4,5]
+print add(a)
---
+++ calculator_2.py
@@ -0,0 +1,55 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+from sys import exc_info
+from arithmetic import *
+
+
+# Your code goes here
+while True:
+    try:
+        user_request = raw_input(" >> ")
+        if user_request == "quit" or user_request == "q":
+            break
+        elif len(user_request) <= 1 or user_request[0] == " ":
+            print "Nice try! Please enter a valid number of inputs."    
+        elif user_request[0:6] == "square" or user_request[0:4] == "cube":
+            if user_request[6] == " " or user_request[4] == " ":
+                tokens = user_request.split()
+                operator,num1 = tokens
+                num1 = int(num1)
+                if operator == "square":
+                    print square(num1)  
+                elif operator == "cube":
+                    print cube(num1)
+            else:
+                print "Invalid command! Make sure you check your spelling."
+        else:
+            tokens = user_request.split()
+            operator,num1, num2 = tokens
+            num1 = int(num1)
+            num2 = int(num2)
+            if operator == "+":
+                print add(num1, num2)
+            elif operator == "-":
+                print subtract(num1, num2)
+            elif operator == "*":
+                print multiply(num1, num2)
+            elif operator == "/":
+                print divide(num1, num2)
+            elif operator == "pow":
+                print power(num1, num2) 
+            elif operator == "mod":
+                print mod(num1, num2)
+            else:
+                print "Invalid command! Please try again!"      
+    except ValueError:
+        print "Invalid command! Try again!"
+    except:
+        print "Goodbye ! Exiting the calculator !"
+        break   
+            
+        
